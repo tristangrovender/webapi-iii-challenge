d003ec2723d817024b3ec352541ab050fb666f12
Tristan Grovender <therealtristangrovender@gmail.com>
2019-04-25 15:12:33 -0500

    first commit, boilerplate complete

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code away!
+const express = require('express');
+
+const server = express();
+
+server.use('/', (req, res) => {
+    res.status(200).send("It's aliiivvveee!!!");
+});
+
+server.listen(4000, () => {
+    console.log('\n** API up and running on port 4k **');
+});
